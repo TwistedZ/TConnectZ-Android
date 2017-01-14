e511d73d75ceae5c41370be1484cd480f18e357f
Will <blightedsage@gmail.com>
2017-01-14 03:54:31 -0500

    Assign VMC to GUI elements.

--- app/src/main/java/ml/twisted/z/tconnectz/TConnectZ.java
+++ app/src/main/java/ml/twisted/z/tconnectz/TConnectZ.java
@@ -7,11 +7,17 @@ import android.widget.TextView;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import android.widget.*;
 
 public class TConnectZ extends Activity {
+	
+	private EditText messageEditText;
+	private Button publishButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		messageEditText = (EditText) findViewById(R.id.text);
+		publishButton = (Button) findViewById(R.id.publish);
         setContentView(R.layout.activity_tconnect_z);
     }
     private final static String QUEUE_NAME = "MinelantisEx";
